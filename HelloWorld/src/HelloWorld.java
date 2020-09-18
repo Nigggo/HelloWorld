95b8ec584934691938d31a623b924da9f5270d38
NicoHoffmann <NicoHoffmann@0-208-07>
2020-09-18 13:00:36 +0200

    1.0

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,16 @@
 
 public class HelloWorld {
-
+	
+	static int counter=0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		
+		while(counter<100) {
+			if(counter%5==0);
+			else(System.out.println("HelloWorld"));
+			System.out.println("Hello World");
+			System.out.println(counter);
+			counter++;
+		}
 	}
 
 }
