d991773503e80b5070b73eb11d51e5acadc93310
NicoHoffmann <NicoHoffmann@0-208-07>
2020-09-25 13:01:12 +0200

    isPrime

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,18 +1,20 @@
-
 public class HelloWorld {
 	
-	static int counter=1;
+	//static int number = 2
+	static int counter = 2;
+	static boolean isPrime = true;
+	
 	public static void main(String[] args) {
 		
-		while(counter<=100) {
-			if(counter%3==0 && counter%5==0) {System.out.println("FizzBuzz");}
-			else
-				if(counter%3==0) {System.out.println("Fizz");}
-					else
-						if(counter%5==0) {System.out.println("Buzz");}
-							else {System.out.println(counter);}
-			counter++;
+		for(int number=2; number<1000; number++) {
+			while(counter<=number/2 && isPrime==true) {
+				if(counter%counter==0) {isPrime=false;};
+		
+				counter++;			
 		}
+			counter=2;
+			if(isPrime==true) {System.out.println(number);}
+			else{isPrime=true;}
+		} 	
 	}
-
 }
